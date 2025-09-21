ef6d84752f83e646de020da293d341f963695377
hsuncoding0 <hsuncoding0@gmail.com>
2025-09-21 18:57:18 +0800

    AI 修改

--- public/diff.js
+++
@@ -1,5 +0,0 @@
-export function showDiff(oldContent, newContent, containerId) {
-  const diffString = `--- Original\n+++ Updated\n@@ -1 +1 @@\n${oldContent}\n${newContent}`;
-  const diffHtml = Diff2Html.html(diffString, { drawFileList: false, matching: 'lines' });
-  document.getElementById(containerId).innerHTML = diffHtml;
-}
